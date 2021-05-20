c4d2f27de882d1529e39a81073c591cddb98d2ce
Maleke Hanson <71529357+MalekeHan@users.noreply.github.com>
2021-05-20 01:43:30 -0400

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -3,31 +3,42 @@ package classproject;
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Label;
 import java.awt.RenderingHints;
 import java.awt.Stroke;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import java.awt.Container;
 import java.util.Scanner;
+import java.util.Hashtable;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+
+
 
 public class Graph extends JPanel {
 	
 	private static double myAccuracy = 0.0;
 	private static double myPrecision = 0.0;
-	
-	
-	
-	
 	
 
     private static final long serialVersionUID = 1L;
@@ -55,25 +66,11 @@ public class Graph extends JPanel {
     private ArrayList<DataPoint> data;
     
     private KNNPredictor knnPredictor;
-
     
     	
 	/**
 	 * Constructor method
 	 */
-    public Graph(int K, String fileName) {
-        // Generate random data point
-    	
-    	knnPredictor = new KNNPredictor(K);
-    	this.data = knnPredictor.readData(fileName);
-    	
-    	myAccuracy = knnPredictor.getAccuracy(data);
-		myPrecision = knnPredictor.getPrecision(data);
-		
-		myAccuracy *= 100;
-		myPrecision *=100;
-    }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -227,28 +224,90 @@ public class Graph extends JPanel {
     public List<DataPoint> getData() {
         return data;
     }
+    
+    public Graph(int K, String fileName) {
+		redrawKValue(K, fileName);
+	}
+    
+    public void redrawKValue(int K, String fileName) {
+		knnPredictor = new KNNPredictor(K);
+		data = knnPredictor.readData(fileName);
+
+		myAccuracy = knnPredictor.getAccuracy(data) * 100;
+		myPrecision = knnPredictor.getPrecision(data) * 100;
+	}
 
     /*  Run createAndShowGui in the main method, where we create the frame too and pack it in the panel*/
     private static void createAndShowGui(int K, String fileName) {
     	
 	    /* Main panel */
-        Graph mainPanel = new Graph(K, fileName);
+        int value =K;
+        Graph mainPanel = new Graph(value, fileName);
 
         // Feel free to change the size of the panel
-        mainPanel.setPreferredSize(new Dimension(700, 400));
+        mainPanel.setPreferredSize(new Dimension(1000, 700));
 
         /* creating the frame */
-        JFrame frame = new JFrame("CS 112 Lab Part 3");
+        JFrame frame = new JFrame("CS 112 Lab Part 4");
         Container contentPane = frame.getContentPane();
+        
 		JButton accuracy = new JButton("Accuracy: " + String.format("%.2f", myAccuracy) + "%");
 		JButton precision = new JButton("Precision: " + String.format("%.2f", myPrecision) + "%");
+
+		
+        //Slider
+		JLabel label = new JLabel();
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, 2, 25, K);
+		slider.addChangeListener(new ChangeListener() {
+		public void stateChanged(ChangeEvent x) {
+			JSlider firstValue = (JSlider)x.getSource();
+			if (firstValue.getValueIsAdjusting() == false) {
+				int secondValue = firstValue.getValue();
+				secondValue *= 2;
+				secondValue += 1;
+					
+					
+				mainPanel.redrawKValue(secondValue, fileName);
+				mainPanel.repaint();
+				accuracy.setText("Accuracy: " + String.format("%.0f", accuracy) + "%");
+				precision.setText("Precision: " + String.format("%.0f", precision) + "%");
+				}
+			}
+		});
+		 
+	     Hashtable<Integer, JLabel> labels = new Hashtable<>();
+	     labels.put(2, new JLabel("2"));
+	     labels.put(3, new JLabel("3"));
+	     labels.put(5, new JLabel("5"));
+	     labels.put(7, new JLabel("7"));
+	     labels.put(9, new JLabel("9"));
+	     labels.put(11, new JLabel("11"));
+	     labels.put(13, new JLabel("13"));
+	     labels.put(15, new JLabel("15"));
+	     labels.put(17, new JLabel("17"));
+	     labels.put(19, new JLabel("19"));
+	     labels.put(21, new JLabel("21"));
+	     labels.put(23, new JLabel("23"));
+	     labels.put(25, new JLabel("25"));
+	     slider.setLabelTable(labels);
+	     
+	     slider.setMajorTickSpacing(5);
+		 slider.setMinorTickSpacing(1);
+		 slider.setPaintTicks(true);
+	     
+	     
 		contentPane.add(accuracy, BorderLayout.LINE_START);
 		contentPane.add(precision, BorderLayout.LINE_END);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(mainPanel);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+		contentPane.add(label, BorderLayout.PAGE_END);
+		contentPane.add(slider, BorderLayout.AFTER_LAST_LINE);
+		
+		//Put after sliders so that the buttons are included too
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(mainPanel, BorderLayout.PAGE_START);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
         
     }
       
